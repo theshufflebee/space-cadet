81c77fe3962ee36e226c0739f6a4bf29d97deadd
theshufflebee <jonas@schnorchel.li>
2026-03-17 17:04:50 +0100

    updated load SNB function and the script 01 for data loading

--- R/load_snb_data.R
+++ R/load_snb_data.R
@@ -1,34 +1,39 @@
-library(RCurl)
-library(jsonlite)
-library(glue)
+package_loader("RCurl") # API Call
+package_loader("jsonlite") # Handling JSON files
+package_loader("glue") # To bild API Endpoints
 
-load_snb_data <- function(cube = 'zimoma', # refers to the specific dataset in the SNB website
-                          folder = "data", # choose storage folder
-                          file_name = "name", # choose file name
-                          fromDate = "2010-01", # choose start date
+
+load_snb_data <- function(cube = 'zimoma', # refers to the specific data set in the SNB website
+                          folder = "data", # choose storage folder must exist already
+                          file_name = "name", # file saved as name
+                          from_date = "2010-01", # choose start date
                           toDate = "2016-01", # choose end date
                           ids = c('SARON', '3M0'), # Id's can be a string or a vector of strings
                           file_type = 'csv') { # best choice here
-  # This is an adapted version from the SNB's API Calls.
-  # The following webpage is concerned: https://data.snb.ch/en/topics/ziredev/cube/zimoma
   
-  if (!dir.exists(folder)) dir.create(folder)
+  # This is an adapted version from the SNB's API Calls and downloads data and metadata
+  # of a given SNB Dataset and returns a list with the data and metadata
+  # An example dataset is: https://data.snb.ch/en/topics/ziredev/cube/zimoma
   
+  # Prepare the ids for the api call into one string
   collapsed_ids <- paste(ids, collapse = ",")
   
+  # Define save path and name of data
   local_file_name <- file.path(folder, paste0(file_name, ".", file_type))
+  
+  # Define save path and name of metadata
   local_string_file_name <- file.path(folder, paste0(file_name, "_metadata.json"))
   
-  
-  api_url <- glue("https://data.snb.ch/api/cube/{cube}/data/{file_type}/en?dimSel=D0({collapsed_ids})&fromDate={fromDate}&toDate={toDate}")
+  # Create the API Endpoint by inserting information into the url for data and metadata
+  # Values in curly Brackets are replaced
+  api_url <- glue("https://data.snb.ch/api/cube/{cube}/data/{file_type}/en?dimSel=D0({collapsed_ids})&fromDate={from_date}&toDate={toDate}")
   metadata_url <- glue("https://data.snb.ch/api/cube/{cube}/dimensions/en")
   
+  # Download the Data directly into the folder
   download.file(api_url, method = "curl", destfile = local_file_name)
-  raw_data <- read.table(local_file_name, skip=3, header = TRUE, sep=";")
   
-  # Download Meta Data
+  # Download the Metadata directly into the folder
   download.file(metadata_url, method="curl", destfile=local_string_file_name)
-  str_data <- fromJSON(paste(readLines(local_string_file_name, encoding = "UTF-8"), collapse=""))
   
-  return(list(data = raw_data, metadata = str_data))
+
 }
